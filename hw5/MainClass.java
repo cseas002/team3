ea7baf968378c772304e9e909f104061441ac5b2
UNIOrfeas <81628073+UNIOrfeas@users.noreply.github.com>
2021-04-14 19:04:23 +0300

    Update MainClass.java
    Added javadoc

--- hw5/MainClass.java
+++ hw5/MainClass.java
@@ -1,11 +1,24 @@
 package cseas002.team3.hw5;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * The MainClass is the class that provides the main method required to play the game. Upon execution the user is asked wether he wants to play the game with a JFrame or whether he wants to play in console just like in moodle.<br>
+ * 
+ * 
+ * @author Orfeas Pliaridis & Christoforos Seas
+ * @version 1.2
+ */
 public class MainClass {
 
+	/**
+	 * Plays the hangman game. It also asks the user whether he wants to play using a JFrame or using console like in moodle.
+	 * 
+	 * @param args The o-th argument is the name of the File we read our words from, and if he wants to play by console the next 2 arguments are the length of the answer and the amount of Tries the user has.
+	 */
     public static void main(String[] args) {
         //String fileName="dictionary.txt"; int answerLength=9; int amountOfTries=30;/*
         String fileName = args[0];
@@ -27,6 +40,7 @@ public class MainClass {
 
         if (console.equals("yes") || console.equals("Yes") || console.equals("YES")) {
             Hangman.filename = fileName;
+            //System.out.println(new File(fileName).getAbsolutePath());
             new Frame(false, 800, 600) {
                 protected void removeLabels() {
                 }
